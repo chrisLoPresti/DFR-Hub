07cd404ea0b987742967afd564d8bcf450685d6b
Chris LoPresti <lopresti.chris@yahoo.com>
2024-08-03 10:45:43 -0400

    protecting routes

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 export const config = {
     matcher: [
-      '/((?!login|/|api|_next/static|images|favicon.ico).*)',
+      '/((?!login|_next/static|favicon.ico).*)',
     ]}
