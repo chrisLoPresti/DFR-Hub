cc7eaabfabfc440d5532c2d30917a17d3564ff83
chrisLopresti <lopresti.chris@yahoo.com>
2024-08-04 14:08:43 -0400

    rearrange map drawer icons

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,20 @@
 
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 
 export default function Home() {
   const { status } = useSession();
   const router = useRouter();
 
+useEffect(() => {
   if (status === "authenticated") {
     router.push("/map-annotations");
   } else {
     router.push("/login");
   }
+},[])
 
   return <div></div>
 }
